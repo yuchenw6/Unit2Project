2ece54f19482862859b1ec3c4ba5fed636cf9489
yuchenw6 <yuchenw6@nycstudents.net>
2025-11-14 16:27:03 -0500

    Update Main.java
    11/14 added []

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int guess;
 
-        int numList = Game.randomNum();
+        int [] numList = Game.randomNum();
         for (int i = 0; i<5; i++){
             System.out.print("Enter your " + (i+1) + " guess: ");
             guess = scanner.nextInt();
