5bc1ce00380236d6b26910c0ef30565fc93eb6f3
yuchenw6 <yuchenw6@nycstudents.net>
2025-11-19 14:29:22 -0500

    Add files via upload
    11/19/2025 in class 2:29

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,18 @@ public class Main {
         int modeTrial = 5;
         boolean playAgain = true;
         int userPlayAgain;
-        System.out.print("Choose difficulty: 1=Easy, 2=Normal, 3=Hard: ");
+        System.out.print("Choose difficulty: " + "\n" + "1 = Easy (7 tries)" + "\n" + "2 = Normal (6 tries)" + "\n" + "3 = Hard (5 Tries)" + "\n" + "Difficulty: ");
         int mode = scanner.nextInt();
         while (mode < 0 || mode > 3){
-            System.out.print("Re-enter the mode 0-3: ");
+            System.out.print("Re-enter the mode 1-3" + "\n" + "Difficulty: ");
             mode = scanner.nextInt();
             scanner.nextLine();
         }
         System.out.println("----------------------------------------------" + "\n");
         while (playAgain){
-            modeTrial = (6-mode);
+            modeTrial = (8-mode);
             System.out.println("Round: " + round + "\n");
-            System.out.println("Current Mode: " + mode);
+            System.out.println("Current Difficulty: " + mode);
             while (point < 3 && trial < modeTrial) {
                 System.out.print("Enter your " + (trial+1) + " guess: ");
                 guess = scanner.nextInt();
@@ -45,7 +45,7 @@ public class Main {
             }
 
             Game.gamePoint(point, totalPoint, trial, round);
-            System.out.print("Click 1 to Play Again, 0 to Quit: ");
+            System.out.print("Click any key but 0 to Play Again, 0 to Quit: ");
             userPlayAgain = scanner.nextInt();
             scanner.nextLine();
             System.out.println();
@@ -54,12 +54,14 @@ public class Main {
                 System.out.println("GAME OVER");
                 totalPoint = 0;
             } else {
-                System.out.print("Enter the mode you want for the new game: ");
+                System.out.print("Enter the mode you want for the new game" + "\n" + "Difficulty: ");
                 mode = scanner.nextInt();
                 while (mode < 0 || mode > 3){
-                    System.out.print("Re-enter the mode 0-3: ");
+                    System.out.print("Re-enter the mode 1-3" + "\n" + "Difficulty: ");
                     mode = scanner.nextInt();
                     scanner.nextLine();
+                    System.out.println();
+                    System.out.println("----------------------------------------------" + "\n");
                 }
                 point = 0;
                 trial = 0;
